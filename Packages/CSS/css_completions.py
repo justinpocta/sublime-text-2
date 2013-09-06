bc4af4a0523c15591df4f7ca9ab5ad50d3c5e1d4
Justin Pocta <jpocta@zipments.com>
2013-09-05 22:30:00 -0400

    text overflow

--- Packages/CSS/css_completions.py
+++ Packages/CSS/css_completions.py
@@ -98,6 +98,7 @@ css_data = """
 "text-align"=left | right | center | justify | inherit
 "text-decoration"=none | underline | overline | line-through | blink | inherit | none
 "text-indent"=<length> | <percentage> | inherit
+"text-overflow"=clip | ellipsis
 "text-transform"=capitalize | uppercase | lowercase | none | inherit
 "top"=<length> | <percentage> | auto | inherit
 "unicode-bidi"=normal | embed | bidi-override | inherit
